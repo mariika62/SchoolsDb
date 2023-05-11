37b1d1aad0715e00eaa6d2c540f7ea6f83f2c1f4
Todor Demirov <teemo1514@gmail.com>
2023-05-11 20:35:18 +0300

    Testing Layer part2

---
+++ SchoolsDb/TestingLayer/ClassContextTest.cs
@@ -0,0 +1,116 @@
+﻿using BusinessLayer;
+using DataLayer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingLayer
+{
+    [TestFixture]
+    public class ClassContextTest
+    {
+        private ClassContext context = new ClassContext(SetupFixture.dbContext);
+        private Class class_;
+        private Student st1, st2;
+        private School sc1;
+
+        [SetUp]
+        public void CreateClass()
+        {
+            sc1 = new("Akad. K. Popov", "Chemshir 11", "High School");
+            class_ = new("11j", "Liliana Rusinova", sc1);
+            st1 = new("Kristiyan Angelov", 17, 5.80, 14, sc1, class_);
+            st2 = new("Todor Demirov", 15, 5.50, 25, sc1, class_);
+
+            class_.Students.Add(st1);
+            class_.Students.Add(st2);
+
+            context.Create(class_);
+        }
+
+        [TearDown]
+        public void DropClass()
+        {
+            foreach (Class item in SetupFixture.dbContext.Classes.ToList())
+            {
+                SetupFixture.dbContext.Classes.Remove(item);
+            }
+
+            SetupFixture.dbContext.SaveChanges();
+        }
+
+        [Test]
+        public void Create()
+        {
+            Class testClass = new("11e", "Ivan Iliev", sc1);
+            int classBefore = SetupFixture.dbContext.Classes.Count();
+            context.Create(testClass);
+            int classAfter = SetupFixture.dbContext.Classes.Count();
+            Assert.That(classBefore + 1 == classAfter, "Create() does not work!");
+        }
+
+        [Test]
+        public void Read()
+        {
+            Class readClass = context.Read(class_.Id);
+            Assert.AreEqual(class_, readClass, "Read does not return the same object!");
+        }
+
+        [Test]
+        public void ReadWithNavigationalProperties()
+        {
+            Class readClass = context.Read(class_.Id, true);
+            Assert.That(readClass.Students.Contains(st1) && readClass.Students.Contains(st1), "S1 and S2 are not in the Students list!");
+        }
+
+        [Test]
+        public void ReadAll()
+        {
+            List<Class> classes = (List<Class>)context.ReadAll();
+
+            Assert.That(classes.Count != 0, "ReadAll() does not return classes!");
+        }
+
+        [Test]
+        public void ReadAllWithNavigationalProperties()
+        {
+            School sc = new("Dragan Manchov", "Chemshir 11", "Elementary");
+            Class readClass = new("12j", "Unknown", sc);
+            Student st = new("John Doe", 15, 4.50, 15, sc, readClass);
+            SetupFixture.dbContext.Schools.Add(sc);
+            SetupFixture.dbContext.Students.Add(st);
+            SetupFixture.dbContext.Classes.Add(readClass);
+
+            List<Class> classes = (List<Class>)context.ReadAll(true);
+            Assert.That(classes.Count != 0 && context.Read(readClass.Id, true).Students.Count == 1,
+                "ReadAll() does not return classes!");
+        }
+
+        [Test]
+        public void Update()
+        {
+            Class changedClass = context.Read(class_.Id);
+
+            changedClass.Name = "Updated " + class_.Name;
+            changedClass.ClassTeacher = "Denitsa Grozeva";
+
+            context.Update(changedClass);
+
+            Assert.AreEqual(changedClass, class_, "Update() does not work!");
+        }
+
+        [Test]
+        public void Delete()
+        {
+            int classBefore = SetupFixture.dbContext.Classes.Count();
+
+            context.Delete(class_.Id);
+            int classAfter = SetupFixture.dbContext.Classes.Count();
+
+            Assert.IsTrue(classBefore - 1 == classAfter, "Delete() does not work!");
+        }
+    }
+}
---
+++ SchoolsDb/TestingLayer/StudentContextTest.cs
@@ -0,0 +1,87 @@
+﻿using BusinessLayer;
+using DataLayer;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingLayer
+{
+    [TestFixture]
+    public class StudentContextTest
+    {
+        private StudentsContext context = new StudentContextTest(SetupFixture.dbContext);
+        private Student student;
+        private Class cl1;
+        private School sc1;
+
+        [SetUp]
+        public void CreateStudent()
+        {
+            sc1 = new("Akad. K. Popov", "Chemshir 11", "High School");
+            cl1 = new("11j", "Liliana Rusinova", sc1);
+
+            student = new("Todor Demirov", 15, 5.50, sc1, cl1);
+
+            context.Create(student);
+        }
+
+        [TearDown]
+        public void DropStudent()
+        {
+            foreach (Student item in SetupFixture.dbContext.Students.ToList())
+            {
+                SetupFixture.dbContext.Students.Remove(item);
+            }
+            SetupFixture.dbContext.SaveChanges();
+        }
+
+        [Test]
+        public void Create()
+        {
+            Student newStudent = new("Maria Petrova", 17, 5.99, 17, sc1, cl1);
+            int studentsBefore = SetupFixture.dbContext.Students.Count();
+            context.Create(newStudent);
+            int studentsAfter = SetupFixture.dbContext.Students.Count();
+            Assert.IsTrue(studentsBefore + 1 == studentsAfter, "Create() does not work!");
+        }
+
+        [Test]
+        public void Read()
+        {
+            Student readStudent = context.Read(student.Id);
+            Assert.AreEqual(student, readStudent, "Read() does not return the same object");
+        }
+
+        [Test]
+        public void ReadAll()
+        {
+            List<Student> students = (List<Student>)context.ReadAll();
+            Assert.That(students.Count != 0, "ReadAll() does not return students!");
+        }
+
+        [Test]
+        public void Update()
+        {
+            Student changedStudent = context.Read(student.Id);
+            changedStudent.Name = "Updated " + student.Name;
+            changedStudent.Age = 18;
+            context.Update(changedStudent);
+            Assert.AreEqual(changedStudent, student, "Update() does not work!");
+        }
+
+        [Test]
+        public void Delete()
+        {
+            int studentBefore = SetupFixture.dbContext.Students.Count();
+
+            context.Delete(student.Id);
+            int studentAfter = SetupFixture.dbContext.Students.Count();
+
+            Assert.IsTrue(studentBefore - 1 == studentAfter, "Delete() does not work!");
+        }
+    }
+}

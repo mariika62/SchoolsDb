0ce8d8cf00f95d9209a625fff04a8ba19369c1ff
Ralitsa2008 <ralitsatoteva_zh19@schoolmath.eu>
2023-05-11 07:56:58 +0300

    Added DataLayer

---
+++ SchoolsDb/DataLayer/SchoolsContext.cs
@@ -0,0 +1,151 @@
+﻿using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class SchoolsContext : IDb<School, string>
+    {
+        private readonly SchoolsDbContext dbContext;
+
+        public SchoolsContext(SchoolsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(School item)
+        {
+            try
+            {
+                dbContext.Schools.Add(item);
+                dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(string key)
+        {
+            School schoolFromDb = Read(key);
+
+            if (schoolFromDb != null)
+            {
+                dbContext.Schools.Remove(schoolFromDb);
+                dbContext.SaveChanges();
+            }
+            else
+            {
+                throw new InvalidOperationException("School with that id does not exist!");
+            }
+        }
+
+        public School Read(string key, bool useNavigationalProperties = false)
+        {
+            try
+            {
+                IQueryable<School> query = dbContext.Schools;
+
+                if (useNavigationalProperties)
+                {
+                    query = query.Include(s => s.Students)
+                        .Include(s => s.Classes);
+                }
+
+                return query.FirstOrDefault(s => s.Id == key);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IEnumerable<School> ReadAll(bool useNavigationalProperties = false)
+        {
+
+            try
+            {
+                IQueryable<School> query = dbContext.Schools;
+
+                if (useNavigationalProperties)
+                {
+                    query = query.Include(s => s.Students)
+                        .Include(s => s.Classes);
+                }
+
+                return query.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Update(School item, bool useNavigationalProperties = false)
+        {
+            try
+            {
+                School schoolFromDb = Read(item.Id, useNavigationalProperties);
+
+                if (schoolFromDb == null)
+                {
+                    Create(item);
+                    return;
+                }
+
+                schoolFromDb.Address = item.Address;
+                schoolFromDb.Name = item.Name;
+                schoolFromDb.Type = item.Type;
+
+                if (useNavigationalProperties)
+                {
+
+                    List<Student> students = new List<Student>();
+
+                    foreach (Student s in item.Students)
+                    {
+                        Student sFromDb = dbContext.Students.Find(s.Id);
+
+                        if (sFromDb != null)
+                        {
+                            students.Add(sFromDb);
+                        }
+                        else
+                        {
+                            students.Add(s);
+                        }
+                    }
+
+                    List<Class> classes = new List<Class>();
+
+                    foreach (Class c in item.Classes)
+                    {
+                        Class cFromDb = dbContext.Classes.Find(c.Id);
+
+                        if (cFromDb != null)
+                        {
+                            classes.Add(cFromDb);
+                        }
+                        else
+                        {
+                            classes.Add(c);
+                        }
+                    }
+                    schoolFromDb.Students = students;
+                    schoolFromDb.Classes = classes;
+                }
+
+                dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ SchoolsDb/DataLayer/StudentsContext.cs
@@ -0,0 +1,151 @@
+﻿using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class StudentsContext : IDb<Student, string>
+    {
+        private readonly SchoolsDbContext dbContext;
+
+        public StudentsContext(SchoolsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(Student item)
+        {
+            try
+            {
+                School schoolFromDb = dbContext.Schools.Find(item.SchoolId);
+
+                if (schoolFromDb != null)
+                {
+                    item.School = schoolFromDb;
+                }
+
+                Class classFromDb = dbContext.Classes.Find(item.ClassId);
+
+                if (classFromDb != null)
+                {
+                    item.Class = classFromDb;
+                }
+                dbContext.Students.Add(item);
+                dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(string key)
+        {
+            Student studentFromDb = Read(key);
+
+            if (studentFromDb != null)
+            {
+                dbContext.Students.Remove(studentFromDb);
+                dbContext.SaveChanges();
+            }
+            else
+            {
+                throw new InvalidOperationException("Student with that id does not exist!");
+            }
+        }
+
+        public Student Read(string key, bool useNavigationalProperties = false)
+        {
+            try
+            {
+                IQueryable<Student> query = dbContext.Students;
+
+                if (useNavigationalProperties)
+                {
+                    query = query.Include(s => s.School)
+                        .Include(s => s.Class);
+                }
+
+                return query.FirstOrDefault(s => s.Id == key);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IEnumerable<Student> ReadAll(bool useNavigationalProperties = false)
+        {
+            try
+            {
+                IQueryable<Student> query = dbContext.Students;
+
+                if (useNavigationalProperties)
+                {
+                    query = query.Include(s => s.School)
+                        .Include(s => s.Class);
+                }
+
+                return query.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Update(Student item, bool useNavigationalProperties = false)
+        {
+            try
+            {
+                Student studentFromDb = Read(item.Id, useNavigationalProperties);
+
+                if (studentFromDb == null)
+                {
+                    Create(item);
+                    return;
+                }
+
+                studentFromDb.Age = item.Age;
+                studentFromDb.Name = item.Name;
+                studentFromDb.AverageGrade = item.AverageGrade;
+                studentFromDb.Telephone = item.Telephone;
+                studentFromDb.NumberInClass = item.NumberInClass;
+
+                if (useNavigationalProperties)
+                {
+                    School schoolFromDb = dbContext.Schools.Find(item.SchoolId);
+
+                    if (schoolFromDb != null)
+                    {
+                        studentFromDb.School = schoolFromDb;
+                    }
+                    else
+                    {
+                        studentFromDb.School = item.School;
+                    }
+                    Class classFromDb = dbContext.Classes.Find(item.ClassId);
+
+                    if (classFromDb != null)
+                    {
+                        studentFromDb.Class = classFromDb;
+                    }
+                    else
+                    {
+                        studentFromDb.Class = item.Class;
+                    }
+                }
+
+                dbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}

1b8445e07aad3e3d54ea430ed195b0f99cb63214
mariika62 <mariyapetrova_zh19@schoolmath.eu>
2023-06-15 00:25:53 +0300

    student form

--- SchoolsDb/Presentation Layer/StudentForm.Designer.cs
+++ SchoolsDb/Presentation Layer/StudentForm.Designer.cs
@@ -40,23 +40,26 @@ namespace Presentation_Layer
             this.deleteBtn = new System.Windows.Forms.Button();
             this.clearBtn = new System.Windows.Forms.Button();
             this.closeBtn = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.studentsDataGridView = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nameTxtBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.phTxtBox = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.schoolLB = new System.Windows.Forms.ListBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.classLB = new System.Windows.Forms.ListBox();
             this.label8 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.ageNUAD = new System.Windows.Forms.NumericUpDown();
+            this.avgNUAD = new System.Windows.Forms.NumericUpDown();
+            this.nicNUAD = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ageNUAD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avgNUAD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nicNUAD)).BeginInit();
             this.SuspendLayout();
             // 
             // updateBtn
@@ -109,16 +112,16 @@ namespace Presentation_Layer
             this.closeBtn.UseVisualStyleBackColor = true;
             this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
             // 
-            // dataGridView1
+            // studentsDataGridView
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(629, 184);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(300, 188);
-            this.dataGridView1.TabIndex = 5;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.studentsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentsDataGridView.Location = new System.Drawing.Point(629, 184);
+            this.studentsDataGridView.Name = "studentsDataGridView";
+            this.studentsDataGridView.RowHeadersWidth = 51;
+            this.studentsDataGridView.RowTemplate.Height = 29;
+            this.studentsDataGridView.Size = new System.Drawing.Size(300, 188);
+            this.studentsDataGridView.TabIndex = 5;
+            this.studentsDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label1
             // 
@@ -129,13 +132,12 @@ namespace Presentation_Layer
             this.label1.TabIndex = 6;
             this.label1.Text = "Students";
             // 
-            // textBox1
+            // nameTxtBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(42, 65);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(207, 27);
-            this.textBox1.TabIndex = 7;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.nameTxtBox.Location = new System.Drawing.Point(42, 65);
+            this.nameTxtBox.Name = "nameTxtBox";
+            this.nameTxtBox.Size = new System.Drawing.Size(207, 27);
+            this.nameTxtBox.TabIndex = 7;
             // 
             // label2
             // 
@@ -146,14 +148,6 @@ namespace Presentation_Layer
             this.label2.TabIndex = 8;
             this.label2.Text = "Name";
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(42, 138);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(207, 27);
-            this.textBox2.TabIndex = 9;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
@@ -162,14 +156,6 @@ namespace Presentation_Layer
             this.label3.Size = new System.Drawing.Size(36, 20);
             this.label3.TabIndex = 10;
             this.label3.Text = "Age";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(336, 65);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(207, 27);
-            this.textBox3.TabIndex = 11;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label4
             // 
@@ -180,13 +166,12 @@ namespace Presentation_Layer
             this.label4.TabIndex = 12;
             this.label4.Text = "Avarage Grade";
             // 
-            // textBox4
+            // phTxtBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(336, 138);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(207, 27);
-            this.textBox4.TabIndex = 13;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.phTxtBox.Location = new System.Drawing.Point(336, 138);
+            this.phTxtBox.Name = "phTxtBox";
+            this.phTxtBox.Size = new System.Drawing.Size(207, 27);
+            this.phTxtBox.TabIndex = 13;
             // 
             // label5
             // 
@@ -197,14 +182,6 @@ namespace Presentation_Layer
             this.label5.TabIndex = 14;
             this.label5.Text = "Telephone";
             // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(625, 65);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(207, 27);
-            this.textBox5.TabIndex = 15;
-            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -214,15 +191,15 @@ namespace Presentation_Layer
             this.label6.TabIndex = 16;
             this.label6.Text = "Number in class";
             // 
-            // listBox1
+            // schoolLB
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(37, 242);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(212, 164);
-            this.listBox1.TabIndex = 17;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.schoolLB.FormattingEnabled = true;
+            this.schoolLB.ItemHeight = 20;
+            this.schoolLB.Location = new System.Drawing.Point(37, 242);
+            this.schoolLB.Name = "schoolLB";
+            this.schoolLB.Size = new System.Drawing.Size(212, 164);
+            this.schoolLB.TabIndex = 17;
+            this.schoolLB.SelectedIndexChanged += new System.EventHandler(this.schoolLB_SelectedIndexChanged);
             // 
             // label7
             // 
@@ -233,15 +210,15 @@ namespace Presentation_Layer
             this.label7.TabIndex = 18;
             this.label7.Text = "School";
             // 
-            // listBox2
+            // classLB
             // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 20;
-            this.listBox2.Location = new System.Drawing.Point(336, 242);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(207, 164);
-            this.listBox2.TabIndex = 19;
-            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            this.classLB.FormattingEnabled = true;
+            this.classLB.ItemHeight = 20;
+            this.classLB.Location = new System.Drawing.Point(336, 242);
+            this.classLB.Name = "classLB";
+            this.classLB.Size = new System.Drawing.Size(207, 164);
+            this.classLB.TabIndex = 19;
+            this.classLB.SelectedIndexChanged += new System.EventHandler(this.classLB_SelectedIndexChanged);
             // 
             // label8
             // 
@@ -252,27 +229,48 @@ namespace Presentation_Layer
             this.label8.TabIndex = 20;
             this.label8.Text = "Class";
             // 
+            // ageNUAD
+            // 
+            this.ageNUAD.Location = new System.Drawing.Point(42, 140);
+            this.ageNUAD.Name = "ageNUAD";
+            this.ageNUAD.Size = new System.Drawing.Size(207, 27);
+            this.ageNUAD.TabIndex = 21;
+            // 
+            // avgNUAD
+            // 
+            this.avgNUAD.Location = new System.Drawing.Point(336, 66);
+            this.avgNUAD.Name = "avgNUAD";
+            this.avgNUAD.Size = new System.Drawing.Size(207, 27);
+            this.avgNUAD.TabIndex = 22;
+            // 
+            // nicNUAD
+            // 
+            this.nicNUAD.Location = new System.Drawing.Point(625, 65);
+            this.nicNUAD.Name = "nicNUAD";
+            this.nicNUAD.Size = new System.Drawing.Size(207, 27);
+            this.nicNUAD.TabIndex = 23;
+            // 
             // StudentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(970, 512);
+            this.Controls.Add(this.nicNUAD);
+            this.Controls.Add(this.avgNUAD);
+            this.Controls.Add(this.ageNUAD);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.classLB);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.schoolLB);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.phTxtBox);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.nameTxtBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.studentsDataGridView);
             this.Controls.Add(this.closeBtn);
             this.Controls.Add(this.clearBtn);
             this.Controls.Add(this.deleteBtn);
@@ -280,7 +278,10 @@ namespace Presentation_Layer
             this.Controls.Add(this.updateBtn);
             this.Name = "StudentForm";
             this.Text = "StudentForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ageNUAD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.avgNUAD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nicNUAD)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -293,21 +294,21 @@ namespace Presentation_Layer
         private Button deleteBtn;
         private Button clearBtn;
         private Button closeBtn;
-        private DataGridView dataGridView1;
+        private DataGridView studentsDataGridView;
         private Label label1;
-        private TextBox textBox1;
+        private TextBox nameTxtBox;
         private Label label2;
-        private TextBox textBox2;
         private Label label3;
-        private TextBox textBox3;
         private Label label4;
-        private TextBox textBox4;
+        private TextBox phTxtBox;
         private Label label5;
-        private TextBox textBox5;
         private Label label6;
-        private ListBox listBox1;
+        private ListBox schoolLB;
         private Label label7;
-        private ListBox listBox2;
+        private ListBox classLB;
         private Label label8;
+        private NumericUpDown ageNUAD;
+        private NumericUpDown avgNUAD;
+        private NumericUpDown nicNUAD;
     }
 }
--- SchoolsDb/Presentation Layer/StudentForm.cs
+++ SchoolsDb/Presentation Layer/StudentForm.cs
@@ -14,32 +14,33 @@ namespace Presentation_Layer
 {
     public partial class StudentForm : Form
     {
-        private DbManager<Student, string> dbManager = new DbManager<Student, string>(ContextGenerator.GetBrandsContext());
+        private DbManager<Student, string> dbManager = new DbManager<Student, string>(ContextGenerator.GetStudentsContext());
         private Student selectedStudent;
+        private School selectedSchool;
+        private Class selectedClass;
+        private DbManager<School, string> schoolManager = new DbManager<School, string>(ContextGenerator.GetSchoolsContext());
+        private DbManager<Class, string> classManager = new DbManager<Class, string>(ContextGenerator.GetClassContext());
 
         public StudentForm()
         {
             InitializeComponent();
-            LoadBrands();
-            brandsDataGridView.ReadOnly = true;
+            LoadStudents();
+            studentsDataGridView.ReadOnly = true;
         }
-
-        #region Events
-
 
         private void updateBtn_Click(object sender, EventArgs e)
         {
             try
             {
                 if (ValidationManager.IsValidString(nameTxtBox.Text)
-                    && ValidationManager.IsValidString(emailTxtBox.Text)
-                    && ValidationManager.IsValidString(phoneTxtBox.Text)
+                    && ValidationManager.IsValidString(phTxtBox.Text)
                     && selectedStudent != null)
                 {
                     selectedStudent.Name = nameTxtBox.Text;
-                    selectedStudent.Email = emailTxtBox.Text;
-                    selectedStudent.Phone = phoneTxtBox.Text;
-                    selectedStudent.Address = addressTxtBox.Text;
+                    selectedStudent.AverageGrade = Convert.ToDouble(avgNUAD.Value);
+                    selectedStudent.NumberInClass = Convert.ToInt32(nicNUAD.Value);
+                    selectedStudent.Age = Convert.ToInt32(ageNUAD.Value);
+                    selectedStudent.Telephone = phTxtBox.Text;
 
                     dbManager.Update(selectedStudent);
 
@@ -59,7 +60,41 @@ namespace Presentation_Layer
         }
         private void createBtn_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (ValidationManager.IsValidString(nameTxtBox.Text)
+                    && ValidationManager.IsValidString(phTxtBox.Text))
+                {
+                    string name = nameTxtBox.Text;
+                    double averageGrade = Convert.ToDouble(avgNUAD.Value);
+                    int numberInclass = Convert.ToInt32(nicNUAD.Value);
+                    int age = Convert.ToInt32(ageNUAD.Value);
+                    string telephone = phTxtBox.Text;
+                    School? school = schoolLB.SelectedItem as School;
+                    Class? @class= classLB.SelectedItem as Class;
 
+                    //string name, int age, double averageGrade, int numberInClass, School school, Class @class, string telephone = null
+                    Student student = new Student(name, age, averageGrade, numberInclass, school, @class);
+                    dbManager.Create(student);
+
+                    MessageBox.Show("Student created successfully! :)", "😎", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    LoadStudents();
+
+                    ClearTextBoxes();
+
+                    nameTxtBox.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("You have to enter values in the text boxes! 🤨🤨🤨", "☹", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    nameTxtBox.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "😭", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
         private void deleteBtn_Click(object sender, EventArgs e)
         {
@@ -83,6 +118,16 @@ namespace Presentation_Layer
                 MessageBox.Show(ex.Message, "😭", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0)
+            {
+                MessageBox.Show("🔥");
+                return;
+            }
+            selectedStudent = studentsDataGridView.Rows[e.RowIndex].DataBoundItem as Student;
+            FillTextBoxes();
+        }
         private void clearBtn_Click(object sender, EventArgs e)
         {
             selectedStudent = null;
@@ -92,45 +137,60 @@ namespace Presentation_Layer
         {
             this.Close();
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void ClearTextBoxes()
         {
-
+            nameTxtBox.Text = string.Empty;
+            avgNUAD.Value = 0;
+            nicNUAD.Value = 0;
+            phTxtBox.Text = string.Empty;
+            ageNUAD.Value = 0;
+            schoolLB.SelectedIndex = -1;
+            classLB.SelectedIndex = -1;
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        private void FillTextBoxes()
         {
-
+            if (selectedStudent != null)
+            {
+                nameTxtBox.Text = selectedStudent.Name;
+                ageNUAD.Value = Convert.ToInt32(selectedStudent.Age);
+                avgNUAD.Value = (decimal)selectedStudent.AverageGrade;
+                phTxtBox.Text = selectedStudent.Telephone;
+                nicNUAD.Value = Convert.ToInt32(selectedStudent.NumberInClass);
+                schoolLB.SelectedItem = selectedStudent.School;
+                classLB.SelectedItem = selectedStudent.Class;
+            }
         }
 
-        private void textBox5_TextChanged(object sender, EventArgs e)
+        private void LoadStudents()
         {
-
+            studentsDataGridView.DataSource = dbManager.ReadAll();
+        }
+        public void LoadSchools()
+        {
+            schoolLB.DataSource = schoolManager.ReadAll();
+            schoolLB.DisplayMember = "Name";
+        }
+        public void LoadClass()
+        {
+            classLB.DataSource = classManager.ReadAll();
+            classLB.DisplayMember = "Name";
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void schoolLB_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            if (schoolLB.SelectedIndex >= 0 && schoolLB.SelectedIndex < schoolLB.Items.Count)
+            {
+                selectedSchool = schoolLB.Items[schoolLB.SelectedIndex] as School;
+            }
         }
 
-        private void textBox4_TextChanged(object sender, EventArgs e)
+        private void classLB_SelectedIndexChanged(object sender, EventArgs e)
         {
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
+            if (classLB.SelectedIndex >= 0 && classLB.SelectedIndex < classLB.Items.Count)
+            {
+                selectedClass = classLB.Items[classLB.SelectedIndex] as Class;
+            }
         }
     }
 }
